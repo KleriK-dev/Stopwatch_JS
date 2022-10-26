79537779cc454744e1a0ab9749343db83a3870eb
Klerido <biciklerido@gmail.com>
2022-10-26 16:49:41 +0300

    TypeScript added to the project

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let wra = 0;
+if (wra < 12) {
+    wra += 10;
+}
+console.log("DINW ZWI EDW PERA");
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let wra: number = 0;
+
+if(wra < 12){
+    wra += 10;
+}
+
+console.log("DINW ZWI EDW PERA");
